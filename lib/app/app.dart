347a25736755582f7c2997ffddfe5ca8611a95ea
Harish Vadaparty <harishvadaparty@gmail.com>
2022-04-21 17:42:22 +0530

    initial commit

---
+++ lib/app/app.dart
@@ -0,0 +1,17 @@
+export 'utils.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class DocPadApp extends ConsumerStatefulWidget {
+  const DocPadApp({Key? key}) : super(key: key);
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _DocPadAppState();
+}
+
+class _DocPadAppState extends ConsumerState<DocPadApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'app/app.dart';
+
+void main() {
+  setupLogger();
+
+  runApp(const ProviderScope(child: DocPadApp()));
+}
